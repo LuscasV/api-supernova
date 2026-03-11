dce90d2af85430abdb5b8db962bde67bd577a05c
LuscasV <luscas.pessoal19@gmail.com>
2026-03-11 14:58:43 -0300

    feat: cancel order

--- orders/urls.py
+++ orders/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import OrderCreateView, MyOrdersListView, OrderDetailView
+from .views import OrderCreateView, MyOrdersListView, OrderDetailView, CancelOrderView
 
 urlpatterns = [
     path("create/", OrderCreateView.as_view(), name="order-create"),
     path("my-orders/", MyOrdersListView.as_view(), name="my-orders"),
     path("<int:pk>/", OrderDetailView.as_view(), name="order-detail"),
+    path("<int:pk>/cancel/", CancelOrderView.as_view(), name="order-cancel"),
 ]
