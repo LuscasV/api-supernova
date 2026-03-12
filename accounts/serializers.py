243f097335be1c4674504b018661ae35416a43b6
LuscasV <luscas.pessoal19@gmail.com>
2026-03-12 11:02:40 -0300

    feat: cancel order

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,7 +1,7 @@
-from .models import Profile
+from .models import Profile, WishlistItem, Address
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
-from .models import Address
+from products.models import Product, Size, Color
 
 User = get_user_model()
 
@@ -78,3 +78,62 @@ class AddressSerializer(serializers.ModelSerializer):
             Address.objects.filter(user=user, is_default=True).exclude(id=instance.id).update(is_default=False)
 
         return super().update(instance, validated_data)
+
+# serializer da wishlist
+class WishlistSerializer(serializers.ModelSerializer):
+    product_id = serializers.IntegerField(source="product.id", read_only=True)
+    product_name = serializers.CharField(source="product.name", read_only=True)
+    product_price = serializers.DecimalField(source="product.price", max_digits=10, decimal_places=2, read_only=True)
+
+    size = serializers.CharField(source="size.name", read_only=True)
+    color = serializers.CharField(source="color.name", read_only=True)
+    color_hex = serializers.CharField(source="color.hex_code", read_only=True)
+
+    product_image = serializers.SerializerMethodField()
+
+    class Meta:
+        model = WishlistItem
+        fields = [
+            "id",
+            "product_id",
+            "product_name",
+            "product_price",
+            "size",
+            "color",
+            "color_hex",
+            "product_image",
+            "created_at",
+        ]
+    def get_product_image(self, obj):
+        image = obj.product.images.filter(is_main=True).first()
+        if image:
+            return image.image.url
+        return None
+
+class AddWishlistSerializer(serializers.Serializer):
+    product_id = serializers.IntegerField()
+    size_id = serializers.IntegerField(required=False)
+    color_id = serializers.IntegerField(required=False)
+
+    def create(self, validated_data):
+
+        request = self.context["request"]
+        profile = request.user.profile
+        product_id = validated_data["product_id"]
+
+        try:
+            product = Product.objects.get(id=product_id)
+        except Product.DoesNotExist:
+            raise serializers.ValidationError("Produto não encontrado.")
+
+        wishlist_item, created = WishlistItem.objects.get_or_create(
+            profile=profile,
+            product=product,
+        )
+
+        if not created:
+            raise serializers.ValidationError(
+                {"message": "Produto já está na wishlist."}
+            )
+
+        return wishlist_item
