0f9330bb317ae07df25734e27645edb47b065a54
LuscasV <luscas.pessoal19@gmail.com>
2026-04-18 12:12:41 -0300

    fix: login superuser verification

--- accounts/models.py
+++ accounts/models.py
@@ -20,6 +20,7 @@ class UserManager(BaseUserManager):
     def create_superuser(self, email, password=None, **extra_fields):
         extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_verified", True)
 
         if extra_fields.get("is_staff") is not True:
             raise ValueError("Superuser precisa ter is_staff=True")
